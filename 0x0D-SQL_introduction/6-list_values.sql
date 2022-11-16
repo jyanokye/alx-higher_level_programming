da449a825729570ba30994669308ec880394a711
jyanokye <jamesyawanokye17@gmail.com>
2022-11-16 07:07:39 -0800

    stunting

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- a script that lists all rows of the table first_table
+-- from the database hbtn_0c_0 in your MySQL server
+-- All fields should be printed 
+-- the database name will then be passed as an argument of the mysql command
+SELECT * FROM `first_table`;
