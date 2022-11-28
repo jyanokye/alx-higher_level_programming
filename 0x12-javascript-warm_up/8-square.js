f5f108cced501e900f84972e7a186df9c385c996
jyanokye <jamesyawanokye17@gmail.com>
2022-11-28 15:55:30 -0800

    fast

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+let num = parseInt(process.argv[2]);
+if (num) {
+	for (let i = 0; i < num; i++) {
+		console.log('X'.repeat(num));
+	}
+} else if (num < 0) {
+	console.log('');
+} else {
+	console.log('Missing size');
+}\n
