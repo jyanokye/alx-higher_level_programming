141ebce984173cf1ef1179f4bdc22f99cfc3771e
jyanokye <jamesyawanokye17@gmail.com>
2022-12-04 05:19:12 -0800

    gest

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,8 +8,8 @@ from model_state import Base, State
 
 def list_state_obj():
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-            pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
 
@@ -20,6 +20,7 @@ def list_state_obj():
     for i in rows:
         print("{}: {}".format(i.__dict__['id'], i.__dict__['name']))
 
-        session.close()
-        if __name__ == "__main__":
-            list_state_obj()
+    session.close()
+
+if __name__ == "__main__":
+    list_state_obj()
