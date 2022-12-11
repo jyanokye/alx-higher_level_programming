f1e31edc9693e4079241234d4ac562947f0e724c
jyanokye <jamesyawanokye17@gmail.com>
2022-12-11 11:26:59 -0800

    have

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,4 @@
-hows db listing show and genre not linked
+--query hbtn_0d_tvshows db listing show and genre not linked
 SELECT tv_shows.title, tv_show_genres.genre_id
   FROM tv_shows AS tv_shows
 	 LEFT JOIN tv_show_genres AS tv_show_genres
