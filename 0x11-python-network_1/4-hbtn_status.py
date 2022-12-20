66d963fe434f4fd54ad03bed4dc2c2d1b0cd4ed7
Anokye James Yaw <jamesyawanokye17@gmail.com>
2022-12-20 07:47:25 -0800

    runnu

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,12 +7,10 @@ import requests
 
 def status():
     """a Python script that fetches https://intranet.hbtn.io/status"""
-    response = requests.get('https://alx-intranet.hbtn.io/status')
-    return response
 
 
 if __name__ == '__main__':
-    status = get_status()
+    req = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type:", type(status.text))
-    print("\t- content:", status.text)
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
