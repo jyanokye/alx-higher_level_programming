5e30cd110a7dd32f37780cdd188c3799ae9cd80e
Anokye James Yaw <jamesyawanokye17@gmail.com>
2022-12-20 06:45:15 -0800

    landlord

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 
+"""Module containing the find_peak function."""
+
 def find_peak(list_of_integers):
+    """Find a peak in a list of unsorted integers.
     
-    loi = list_of_integers
-    if loi == []:
+    Args:
+        list_of_integers: A list of an unsorted integers.
+    Returns:
+        The first peak found in the list, or None if no peak is found.
+
+    """
+    if not list_of_integers:
         return None
-    total = len(loi)
-    if total == 1:
-        return loi[0]
-    elif total == 2:
-        return max(loi)
-    middle = int(total / 2)
-    peak = loi[middle]
-    if peak > loi[middle - 1] and peak > loi[middle + 1]:
-        return peak
-    elif peak < loi[middle - 1]:
-        return find_peak(loi[:middle])
+
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+
+    middle = len(list_of_integers) // 2
+    if (list_of_integers[middle] > list_of_integers[middle - 1] and
+            list_of_integers[middle] > list_of_integers[middle + 1]):
+        return list_of_integers[middle]
+    elif list_of_integers[middle] < list_of_integers[middle - 1]:
+        return find_peak(list_of_integers[:middle])
     else:
-        return find_peak(loi[middle + 1:])
+        return find_peak(list_of_integers[middle + 1:])
