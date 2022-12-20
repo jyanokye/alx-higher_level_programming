d378f99c6aabbdef71d5b444dcc73f6effa48a3a
Anokye James Yaw <jamesyawanokye17@gmail.com>
2022-12-20 07:04:56 -0800

    atia

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,17 +11,19 @@ def find_peak(list_of_integers):
         The first peak found in the list, or None if no peak is found.
 
     """
-    if not list_of_integers:
+    loi = list_of_integers
+    if loi == []:
         return None
-
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
-
-    middle = len(list_of_integers) // 2
-    if (list_of_integers[middle] > list_of_integers[middle - 1] and
-            list_of_integers[middle] > list_of_integers[middle + 1]):
-        return list_of_integers[middle]
-    elif list_of_integers[middle] < list_of_integers[middle - 1]:
-        return find_peak(list_of_integers[:middle])
+    total = len(loi)
+    if total == 1:
+        return loi[0]
+    elif total == 2:
+        return max(loi)
+    middle = int(total / 2)
+    peak = loi[middle]
+    if peak > loi[middle - 1] and peak > loi[middle + 1]:
+        return peak
+    elif peak < loi[middle - 1]:
+        return find_peak(loi[:middle])
     else:
-        return find_peak(list_of_integers[middle + 1:])
+        return find_peak(loi[middle + 1:])
