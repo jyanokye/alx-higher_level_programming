1a98404f0db70b459797962ae92fbc07a897e612
Anokye James Yaw <jamesyawanokye17@gmail.com>
2022-12-20 08:25:02 -0800

    akyekye

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python3
 """
 Takes in a letter and sends a POST request to a URL and
-Uses the letter as a parameter
+Uses the letter as a parameter also
 """
 import requests
-from sys import argv
-
-
-def json_api():
-        try:
-            q = argv[1]
-        except Valuemistake:
-            q = ''
-        url = 'http://0.0.0.0:5000/search_user'
-        payload = {'q': q}
-        r = requests.post(url, payload)
-        try:
-            json = r.json()
-            if len(json) == 0:
-                print('No result')
-            else:
-                json_id = json.get('id')
-                json_name = json.get('name')
-                print('[{}] {}'.format(json_id, json_name))
-        except ValueError:
-            print('Not a valid JSON')
-
+import sys
 
 if __name__ == '__main__':
-    json_api()
+
+     letter = "" if len(sys.argv) == 1 else sys.argv[1]
+     payload = {"q": letter}
+     req = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+     try:
+         response = req.json()
+         if response == {}:
+             print("No result")
+         else:
+             print("[{}] {}".format(response.get("id"), response.get("name")))
+     except ValueError:
+         print("Not a valid JSON")
