66b28aa386747c35c5fa9464a9c0dad325c543b5
jyanokye <101642075+jyanokye@users.noreply.github.com>
2022-12-23 23:43:47 +0000

    addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a function that adds 2 integers."""
+
+
+def add_integer(a, b=98):
+    """ a and b must be integer or floats """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+
+    return a + b
