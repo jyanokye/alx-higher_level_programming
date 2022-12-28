3ee1b8c79342d5e2a454f5dbabf488030af81af4
jyanokye <101642075+jyanokye@users.noreply.github.com>
2022-12-28 16:01:33 +0000

    Create 4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a function inherits_from(obj, a_class)"""
+
+
+def inherits_from(obj, a_class):
+    """returns True if the object is an instance of a class \
+    that inherited (directly or indirectly) from the \
+    specified class ; otherwise False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    else:
+        return False
