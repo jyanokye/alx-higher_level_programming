7ec4ab71d6076115bf477bd96097cb31a06a4e61
jyanokye <101642075+jyanokye@users.noreply.github.com>
2022-12-28 16:03:52 +0000

    Create 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a class Square that is a subclass of class Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A subclass of rectangle"""
+    def __init__(self, size):
+        """ initializes the object
+
+        Args:
+        size (int): size of the square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
