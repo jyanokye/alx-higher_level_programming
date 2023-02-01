0364011ef54960fef30a071b06d0cf0f520f6842
jyanokye <101642075+jyanokye@users.noreply.github.com>
2023-02-01 00:08:56 +0000

    Update 2-remove_database.sql

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,4 @@
--- deletes the database hbtn_0c_0 in your MySQL server.
-DROP DATABSE IF EXISTS `hbtn_0c_0`;
+-- script that deletes the database hbtn_0c_0 in your MySQL server.
+-- If the database hbtn_0c_0 doesn’t exist, your script should not fail
+-- You are not allowed to use the SELECT or SHOW statements
+DROP DATABASE IF EXISTS hbtn_0c_0;
